3293ff7b8029c2b7f941c18a64a01497431f1384
XuL1gun <118057281+XuL1gun@users.noreply.github.com>
2022-12-03 21:45:11 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	private int _password = 99999;
+	private string _charpass = "*****";
+	private bool _passwordChanged = true;
+
+	public MainPage()
+	{
+		InitializeComponent();
+        DisplayLabel.Text = _charpass;
+		DisplayLabel.Text += "\n";
+    }
+	private void DigitClicked(object sender, EventArgs e)
+	{
+		if (_passwordChanged)
+			DisplayLabel.Text += (sender as Button).Text;
+    }
+
+	private void EqualsClicked(object sender, EventArgs e)
+	{
+		if (_passwordChanged && DisplayLabel.Text.Split("\n").Last() != "")
+		{
+			if ((Convert.ToInt32(DisplayLabel.Text.Split('\n').Last()) == _password))
+			{
+				DisplayLabel.Text = "Пароль введен правильно";
+				_passwordChanged = false;
+			}
+			else
+			{
+				DisplayLabel.Text = "Пароль неверный. Введите заново.";
+				DisplayLabel.Text += "\n";
+				DisplayLabel.Text += _charpass;
+				DisplayLabel.Text += "\n";
+			}
+		}
+	}
+
+	private void ClearClicked(object sender, EventArgs e)
+	{
+		DisplayLabel.Text = _charpass;
+		DisplayLabel.Text += "\n";
+		_passwordChanged = true;
+	}
+}
+
